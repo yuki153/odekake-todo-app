0c83ebc920774e9f3da638cd560e0995ae2e50d1
fujino-yuki <xxx153yuu.story@gmail.com>
2019-01-20 19:55:44 +0900

    modify: Changed for deploy nuxt2 project to firebase.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,33 @@
-import * as functions from 'firebase-functions';
+// import * as functions from 'firebase-functions';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
 //
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+const functions = require('firebase-functions');
+const express = require('express');
+const { Nuxt } = require('nuxt');
+
+const app = express();
+const nuxt = new Nuxt({
+    dev: false,
+    buildDir: 'nuxt',
+    build: {
+        publicPath: '/assets/'
+    }
+});
+
+function handleRequest(req: any, res: any) {
+    res.set('Cache-Control', 'public, max-age=600, s-maxage=1200');
+    return new Promise((resolve, reject) => {
+        nuxt.render(req, res, (promise: any) => {
+            promise.then(resolve).catch(reject);
+        });
+    });
+}
+
+app.use(handleRequest);
+exports.ssrapp = functions.https.onRequest(app);
