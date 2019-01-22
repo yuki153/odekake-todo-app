59f9b50920c57d19bc8b0dddc173e019e372e678
fujino-yuki <xxx153yuu.story@gmail.com>
2019-01-22 20:40:54 +0900

    add: ログイン用のレイアウトファイルを追加

---
+++ src/layouts/login.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <nuxt />
+    <ul class="loginMenu">
+      <li class="loginMenu__item"><nuxt-link to="/sign-in">ログイン画面</nuxt-link></li>
+      <li class="loginMenu__item"><nuxt-link to="/sign-up">新規登録</nuxt-link></li>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss">
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
+
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+
+.loginMenu {
+  list-style: none;
+  display: flex;
+  width: 100%;
+
+  &__item {
+    background-color: #FC471E;
+    width: 50%;
+  }
+}
+</style>
