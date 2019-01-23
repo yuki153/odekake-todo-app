95473e49dbf0f802675e9938b56d7fb396407645
fujino-yuki <xxx153yuu.story@gmail.com>
2019-01-23 15:02:39 +0900

    add: logo component for login screen.

---
+++ src/components/login-logo.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="loginLogo">
+    <div class="loginLogo__logo"></div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+$logoSize1: 128px;
+$logoSize2: 168px;
+
+.loginLogo {
+  position: relative;
+  margin: 0 auto;
+  width: $logoSize1;
+  height: $logoSize1;
+  &__logo {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    height: 70%;
+    width: 70%;
+    background-color: #fc471e;
+    border-radius: 8px;
+  }
+}
+@media screen and (min-width: 370px){
+  .loginLogo {
+    width: $logoSize2;
+    height: $logoSize2;
+  }
+}
+</style>
