2393f48a5efc92daad0ac76fc647314d6b7a9a23
fujino-yuki <xxx153yuu.story@gmail.com>
2019-01-24 20:58:35 +0900

    add: Components of main page.

---
+++ src/components/app-footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="appFooter">
+  </footer>
+</template>
+
+<style lang="scss">
+.appFooter {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  height: 48px;
+  width: 100%;
+  background-color: #fc471e;
+}
+</style>
