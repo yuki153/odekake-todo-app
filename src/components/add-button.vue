9a07967d76cb6f1745a72219a2421b23d7e9a9f2
fujino-yuki <xxx153yuu.story@gmail.com>
2019-01-26 22:21:01 +0900

    add: add-button component.

---
+++ src/components/add-button.vue
@@ -0,0 +1,22 @@
+<template>
+  <button class="addButton"></button>
+</template>
+<script>
+export default {
+  props: {
+  }
+}
+</script>
+<style lang="scss" scoped>
+  .addButton {
+    position: fixed;
+    bottom: 64px;
+    right: 16px;
+    width: 48px;
+    height: 48px;
+    border-radius: 50%;
+    background-color: #fc471e;
+    box-shadow: #ddd 0 3px 6px 0px;
+    outline: none;
+  }
+</style>
