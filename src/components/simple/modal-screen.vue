816401dfaf4a96936ba60e0a723f36a4ec665083
fujino-yuki <xxx153yuu.story@gmail.com>
2019-02-01 20:50:26 +0900

    add: Created modal-screen component in simple directory.

---
+++ src/components/simple/modal-screen.vue
@@ -0,0 +1,40 @@
+<template>
+  <transition name="modal">
+    <div v-if="isShow" class="modal-screen">
+      <slot></slot>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  props: {
+    isShow: Boolean,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.modal-screen {
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  background-color: #000;
+  opacity: .7;
+  z-index: 80;
+  transition: .3s,
+}
+
+// modal (transionタグの効果)開始時の style
+.modal-enter {
+  opacity: 0
+}
+
+// modal (transionタグの効果)終了時の style
+.modal-leave-to {
+  opacity: 0;
+}
+</style>
