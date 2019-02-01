4c5fbf3c11ad733a6a59902fe25655e86f18fe1f
fujino-yuki <xxx153yuu.story@gmail.com>
2019-02-01 20:50:49 +0900

    Created modal-button component in mix directory.

---
+++ src/components/mix/modal-button.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="modalButton">
+    <modal-screen :isShow="isModalState"/>
+    <add-button :isActive="isButtonState" @click.native="toggleModalState"/>
+  </div>
+</template>
+
+<script>
+import AddButton from '~/components/simple/add-button';
+import ModalScreen from '~/components/simple/modal-screen';
+
+export default {
+  components: {
+    AddButton,
+    ModalScreen
+  },
+  data() {
+    return {
+      isModalState: false,
+      isButtonState: false,
+    }
+  },
+  methods: {
+    toggleModalState: function() {
+      this.isModalState = !this.isModalState;
+      this.isButtonState = this.isModalState === true;
+    }
+  }
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
