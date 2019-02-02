b89bf44af842cefcd49095b76bf2a02ac18b6fa9
fujino-yuki <xxx153yuu.story@gmail.com>
2019-02-02 18:25:16 +0900

    modify: Changed to circle-icon component from raw html.

--- src/components/mix/modal-button.vue
+++ src/components/mix/modal-button.vue
@@ -4,18 +4,9 @@
       <div class="iconList">
         <p class="iconList__title">アイコンを選択してください</p>
         <ul class="iconList__items">
-          <li class="iconList__item -lightGreen-01"></li>
-          <li class="iconList__item -green-01"></li>
-          <li class="iconList__item -blue-02"></li>
-          <li class="iconList__item -purple-01"></li>
-          <li class="iconList__item -lightBlue-01"></li>
-          <li class="iconList__item -lightBlue-02"></li>
-          <li class="iconList__item -yellow-01"></li>
-          <li class="iconList__item -orange-01"></li>
-          <li class="iconList__item -orange-02"></li>
-          <li class="iconList__item -red-01"></li>
-          <li class="iconList__item -blue-01"></li>
-          <li class="iconList__item -purple-02"></li>
+          <li v-for="(hexCode, index) of hexCodes" :key="index" class="iconList__item">
+            <circle-icon :color="hexCode" :size="48"/>
+          </li>
         </ul>
       </div>
     </modal-screen>
@@ -26,16 +17,32 @@
 <script>
 import AddButton from '~/components/simple/add-button';
 import ModalScreen from '~/components/simple/modal-screen';
+import CircleIcon from '~/components/simple/circle-icon';
 
 export default {
   components: {
     AddButton,
-    ModalScreen
+    ModalScreen,
+    CircleIcon
   },
   data() {
     return {
       isModalState: false,
       isButtonState: false,
+      hexCodes: [
+        '#81B468',
+        '#4f9b4e',
+        '#889BFF',
+        '#B588FF',
+        '#73BABE',
+        '#90D2E0',
+        '#F1D691',
+        '#F1C591',
+        '#FFA488',
+        '#FF8888',
+        '#92BFF4',
+        '#C587E2'
+      ]
     }
   },
   methods: {
@@ -64,12 +71,8 @@ export default {
   }
 
   &__item {
-    width: 48px;
-    height: 48px;
     margin: 0 16px 16px 0;
-    background-color: #aaa;
     float: left;
-    border-radius: 50%;
 
     &:nth-child(4n) {
       margin-right: 0;
@@ -77,19 +80,6 @@ export default {
     &:nth-last-child(-n+4) {
       margin-bottom: 0;
     }
-
-    &.-lightGreen-01 {background-color: #81B468}
-    &.-green-01 {background-color: #4f9b4e}
-    &.-purple-01 {background-color: #B588FF}
-    &.-purple-02 {background-color: #C587E2}
-    &.-lightBlue-01 {background-color: #73BABE}
-    &.-lightBlue-02 {background-color: #90D2E0}
-    &.-blue-01 {background-color: #92BFF4}
-    &.-blue-02 {background-color: #889BFF}
-    &.-yellow-01 {background-color: #F1D691}
-    &.-orange-01 {background-color: #F1C591}
-    &.-orange-02 {background-color: #FFA488}
-    &.-red-01 {background-color: #FF8888}
   }
 }
 
