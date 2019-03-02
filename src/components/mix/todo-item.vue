b0d1803c714f381f6c07d51ff345e64b343e11c9
fujino-yuki <xxx153yuu.story@gmail.com>
2019-03-03 03:25:46 +0900

    modify: Passed newly text and time info to components.

--- src/components/mix/todo-item.vue
+++ src/components/mix/todo-item.vue
@@ -7,10 +7,13 @@
         :size="32"/>
     </div>
     <div class="todoItem__content">
-      <todo-time/>
+      <todo-time
+      :hour="hour"
+      :min="min"/>
     </div>
     <div class="todoItem__content">
-      <todo-textarea/>
+      <todo-textarea
+        :text="text"/>
     </div>
   </div>
 </template>
@@ -28,6 +31,9 @@ export default {
   props: {
     iconColor: String,
     iconImg: String,
+    text: String,
+    hour: String,
+    min: String,
   }
 };
 </script>
