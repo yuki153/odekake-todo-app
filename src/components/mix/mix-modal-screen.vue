bfad7f142e2b3bb278a9f78a83ed15bcfe8d3444
fujino-yuki <xxx153yuu.story@gmail.com>
2019-03-03 13:31:20 +0900

    modify: Execute conversion to actual svg path from string by circle-icon component.

--- src/components/mix/mix-modal-screen.vue
+++ src/components/mix/mix-modal-screen.vue
@@ -19,19 +19,6 @@ import ModalScreen from '~/components/simple/modal-screen';
 import CircleIcon from '~/components/simple/circle-icon';
 import { mapState } from 'vuex';
 
-import icoHuman from '~/assets/svg/ico-human.svg';
-import icoTrain from '~/assets/svg/ico-train.svg';
-import icoCar from '~/assets/svg/ico-car.svg';
-import icoAirport from '~/assets/svg/ico-airport.svg';
-import icoShip from '~/assets/svg/ico-ship.svg';
-import icoBicycle from '~/assets/svg/ico-bicycle.svg';
-import icoEat from '~/assets/svg/ico-eat.svg';
-import icoBag from '~/assets/svg/ico-bag.svg';
-import icoCompany from '~/assets/svg/ico-company.svg';
-import icoHome from '~/assets/svg/ico-home.svg';
-import icoMoon from '~/assets/svg/ico-moon.svg';
-import icoSun from '~/assets/svg/ico-sun.svg';
-
 export default {
   components: {
     ModalScreen,
@@ -40,18 +27,18 @@ export default {
   data() {
     return {
       icons : [
-        { hexCode: '#81B468', svg: icoHuman },
-        { hexCode: '#4f9b4e', svg: icoTrain },
-        { hexCode: '#889BFF', svg: icoCar },
-        { hexCode: '#B588FF', svg: icoAirport },
-        { hexCode: '#73BABE', svg: icoShip },
-        { hexCode: '#90D2E0', svg: icoBicycle },
-        { hexCode: '#F1D691', svg: icoEat },
-        { hexCode: '#F1C591', svg: icoBag },
-        { hexCode: '#FFA488', svg: icoCompany },
-        { hexCode: '#FF8888', svg: icoHome },
-        { hexCode: '#92BFF4', svg: icoSun },
-        { hexCode: '#C587E2', svg: icoMoon },
+        { hexCode: '#81B468', svg: 'icoHuman' },
+        { hexCode: '#4f9b4e', svg: 'icoTrain' },
+        { hexCode: '#889BFF', svg: 'icoCar' },
+        { hexCode: '#B588FF', svg: 'icoAirport' },
+        { hexCode: '#73BABE', svg: 'icoShip' },
+        { hexCode: '#90D2E0', svg: 'icoBicycle' },
+        { hexCode: '#F1D691', svg: 'icoEat' },
+        { hexCode: '#F1C591', svg: 'icoBag' },
+        { hexCode: '#FFA488', svg: 'icoCompany' },
+        { hexCode: '#FF8888', svg: 'icoHome' },
+        { hexCode: '#92BFF4', svg: 'icoSun' },
+        { hexCode: '#C587E2', svg: 'icoMoon' },
       ]
     }
   },
@@ -64,7 +51,7 @@ export default {
     clickHandler: function(hex, path) {
       this.$store.commit('todo-item/setData', {
         hexCode: hex,
-        svgPath: path,
+        svgName: path,
       });
       this.$store.commit('modal-screen/disableState');
     }
